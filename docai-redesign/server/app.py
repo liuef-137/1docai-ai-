0fa7a309f9a5eae8a4daad31ac3c2c4b44b48fe1
liuef-137 <liuenfu137@foxmail.com>
2026-07-20 23:24:01 +0800

    fix port

--- docai-redesign/server/app.py
+++ docai-redesign/server/app.py
@@ -99,4 +99,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=7777, debug=True)
+    port = int(os.environ.get('PORT', 7777))
+    app.run(host='0.0.0.0', port=port, debug=True)
